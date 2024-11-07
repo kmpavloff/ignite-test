bbc19cfe7b35f7eb7f480082ebca5bac13209c54
Konstantin Pavlov <kmpavloff@yandex.ru>
2024-11-07 23:27:26 +0300

    убрал лишнюю зависимость

--- ignite-test-app/build.gradle.kts
+++ ignite-test-app/build.gradle.kts
@@ -19,7 +19,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.apache.ignite:ignite-spring-boot-thin-client-autoconfigure-ext:1.0.0")
 	implementation("org.apache.ignite:ignite-core:2.16.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
